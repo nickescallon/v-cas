6009ae9b6a09071128c9b3ada1ab3c6bebaa7055
Nick Escallon <nickescallon@gmail.com>
2014-11-02 22:37:56 -0500

    update home page pictures

--- public/js/shared/directives/gallery.directives.js
+++ public/js/shared/directives/gallery.directives.js
@@ -7,6 +7,7 @@
   function neGallery() {
     var directive = {
       restrict: 'E',
+      scope: {},
       replace: true,
       template: '<div class="gallery"><img ng-repeat="picture in pictures" ng-src="{{ picture.url }}"/></div>',
       link: link
@@ -15,12 +16,10 @@
 
     function link(scope, element) {
       scope.pictures = [
-        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_1.jpeg'},
-        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_2.jpeg'},
-        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_3.jpeg'},
+        {url: 'http://veronicacastellucci.com/images/Acardi/acardi_1.jpeg'},
         {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_4.jpeg'},
-        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_5.jpeg'},
-        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_6.jpeg'}
+        {url: 'http://veronicacastellucci.com/images/Gilbert/Gilbert_4.jpeg'},
+        {url: 'http://veronicacastellucci.com/images/Hutchinson/hutchinson_6.jpeg'},
       ];
     };
 
