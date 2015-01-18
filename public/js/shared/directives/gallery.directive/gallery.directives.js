2672e40d71d73e4f565529e21d20cd965bcfb662
Nick Escallon <nickescallon@gmail.com>
2015-01-18 16:58:22 -0500

    adds scroll functionality to gallery

--- public/js/shared/directives/gallery.directive/gallery.directives.js
+++ public/js/shared/directives/gallery.directive/gallery.directives.js
@@ -19,22 +19,60 @@
     return directive;
 
     function link(scope, element) {
-      //TODO: Add scrolling functionality
-      // var galleryElem = element[0].querySelector('.gallery');
-      // galleryElem.scrollLeft += 5;
-      //on hover of right element, += scrollLeft
-      //on hover of left element, -= scrollLeft
+      var scrollLimit;
+      var scrollTimeout;
 
+      // selections
+      var jsElem = element[0];
+      var galleryElem = jsElem.querySelector('.gallery');
+      var scrollLeftElem = jsElem.querySelector('.scroll.left');
+      var scrollRightElem = jsElem.querySelector('.scroll.right');
+
+      // register handlers
+      scrollLeftElem.addEventListener('mouseover', scrollLeft);
+      scrollRightElem.addEventListener('mouseover', scrollRight);
+      scrollLeftElem.addEventListener('mouseout', stopScroll);
+      scrollRightElem.addEventListener('mouseout', stopScroll);
+
+      // scope properties
       scope.displayedImages = [];
+      scope.$watch('images', loadImages);
 
-      scope.$watch('images', function(newVals) {
+      function loadImages() {
         // Here we allow 1 second for the ng-leave animation
         // Also helps to hide preloading
         scope.displayedImages = [];
         $timeout(function() {
           scope.displayedImages = scope.images;
+          scope.scrollLimit = findScrollLimit(galleryElem)
         }, 1000);
-      });
+      };
+
+      function findScrollLimit(element) {
+        return element.scrollWidth - element.clientWidth;
+      };
+
+      function scrollLeft() {
+        if (galleryElem.scrollLeft <= 0) {
+          return;
+        }
+
+        galleryElem.scrollLeft -= 5;
+        scrollTimeout = $timeout(scrollLeft, 10);
+      };
+
+      function scrollRight() {
+        if (galleryElem.scrollLeft >= scrollLimit) {
+          return;
+        }
+
+        galleryElem.scrollLeft += 5;
+        scrollTimeout = $timeout(scrollRight, 10);
+      };
+
+      function stopScroll() {
+        $timeout.cancel(scrollTimeout);
+      };
     };
 
   };
